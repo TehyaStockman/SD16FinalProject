66249e551ec6eaf07d6a015095d6bf1fd81ab12b
Jonathan <Jonathan Jacobs>
2016-04-13 20:14:27 -0400

    this is going to be rough

--- colorTrack.py
+++ colorTrack.py
@@ -20,17 +20,21 @@ class Tracker(object):
 
 		# referencing the webcam for our video camera
 		self.camera = cv2.VideoCapture(0)
-		cv2.namedWindow('image')
-		cv2.createTrackbar('LowR','image',0,255,nothing)
-		cv2.createTrackbar('LowG','image',0,255,nothing)
-		cv2.createTrackbar('LowB','image',0,255,nothing)
-		cv2.createTrackbar('R','image',0,255,nothing)
-		cv2.createTrackbar('G','image',0,255,nothing)
-		cv2.createTrackbar('B','image',0,255,nothing)
+
+		#The following is useful for color calibration
+		# cv2.namedWindow('image')
+		# cv2.createTrackbar('LowR','image',0,255,nothing)
+		# cv2.createTrackbar('LowG','image',0,255,nothing)
+		# cv2.createTrackbar('LowB','image',0,255,nothing)
+		# cv2.createTrackbar('R','image',0,255,nothing)
+		# cv2.createTrackbar('G','image',0,255,nothing)
+		# cv2.createTrackbar('B','image',0,255,nothing)
 	
 
 
 	def track(self):
+		self.center = (0,0)
+
 		while 1:
 			# ret is boolean for successful frame read
 			ret, frame = self.camera.read()
@@ -45,7 +49,6 @@ class Tracker(object):
 
 			# finds the contour in the mask an initializes the storage for ball location
 			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-			self.center = None
 
 			if len(cnts) > 0:
 				# finds the biggest contour and then finds the smallest circle 
@@ -57,12 +60,13 @@ class Tracker(object):
 				M = cv2.moments(c)
 				self.center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
+# This is all commented out because it's extra work not needed for tracking
 				# ensures the thing it's detecting is not too small and a mistake
 				if radius > 1:
 					# draws a circle around our tracked thing. This is mostly for testing to see how 
 					# this ends up working
 					cv2.circle(frame, (int(x),int(y)), int(radius),(0,0,0),2)
-			print self.center
+			
 			# displays the screen 
 			cv2.imshow('Frame',frame)
 			# if we press q key, everything quits
@@ -75,7 +79,6 @@ class Tracker(object):
 			if key == ord('c'):
 				self.colorLow = (cv2.getTrackbarPos('LowB','image'),cv2.getTrackbarPos('LowG','image'),cv2.getTrackbarPos('LowR','image'))
 				self.colorHigh = (cv2.getTrackbarPos('B','image'),cv2.getTrackbarPos('G','image'),cv2.getTrackbarPos('R','image'))
-
 		self.camera.release()
 		cv2.destroyAllWindows()
 
