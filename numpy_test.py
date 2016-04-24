4a701d5d71628d75097cf63b7bd608298a9a3c4d
Mikhaela Dietch <Mikhaela@mikhaelas-mbp.olin.edu>
2016-04-24 17:37:49 -0400

    Some new test files

---
+++ numpy_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+fishes = np.zeros(1,30)
+
+def positions(n):
+	# return np.array([px,py], dtype=np.float64) for px in range(0,n) and py in range(0,n)
+print positions(10)
+
+# velocity = np.array([vx,vy])
+# acceleration = np.array([ax,av])
+
+# motion = np.array([position,velocity,acceleration])
